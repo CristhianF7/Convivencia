8577599507f4cee230dfd25a917d39760da5fff4
Cristhian F <CristhianF7@gmail.com>
2016-05-24 22:04:51 -0500

    inicio login

---
+++ src/AppBundle/Entity/Administrador.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use AppBundle\Models\Persona as BasePersona;
+
+/**
+ * Administrador
+ *
+ * @ORM\Table(name="administrador")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AdministradorRepository")
+ */
+class Administrador extends BasePersona
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
